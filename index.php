343d18722c1d7d349dd6f2895cff11fa15733178
elenaborovik <109816349+elenaborovik@users.noreply.github.com>
2022-08-02 12:32:19 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-echo "Test Beanstalk Auto Upload";
+echo "Test Beanstalk Auto Upload on Jenkins with Beanstalk plugin";
 echo "<h1>Upload using jenkins</h1>";
 echo "And it's successfull!!!!";
